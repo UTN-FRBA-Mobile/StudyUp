b7c56563967dbbd3d58cf37ff89a6a5b816fb54a
chiarama <chiarabetiana@gmail.com>
2022-11-28 18:32:24 -0300

    Add status

--- app/src/main/java/com/studyup/classes/TeamDetail/members/MemberContainer.kt
+++ app/src/main/java/com/studyup/classes/TeamDetail/members/MemberContainer.kt
@@ -8,11 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.studyup.api.Member
-import com.studyup.databinding.FragmentMembersListBinding
-import com.studyup.databinding.FragmentTeamDetailTagsListBinding
+import com.studyup.databinding.FragmentTeamDetailMemberListBinding
 
 class MemberContainer(private var myDatabase: MutableList<Member>? = null): Fragment() {
-    private var _binding: FragmentTeamDetailTagsListBinding? = null
+    private var _binding: FragmentTeamDetailMemberListBinding? = null
     private var viewAdapter: MemberContainerElement? = null
 
     // This property is only valid between onCreateView and
@@ -22,7 +21,7 @@ class MemberContainer(private var myDatabase: MutableList<Member>? = null): Frag
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentTeamDetailTagsListBinding.inflate(inflater, container, false)
+        _binding = FragmentTeamDetailMemberListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
